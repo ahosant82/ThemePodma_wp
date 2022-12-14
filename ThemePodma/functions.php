005e450e548593df039e136a274fbb40ad87f9bd
Ahsanur Rahman Tonmoy <86525672+ahosant82@users.noreply.github.com>
2022-12-14 17:17:02 +0600

    push

--- ThemePodma/functions.php
+++ ThemePodma/functions.php
@@ -1,10 +1,41 @@
 <?php
+wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+wp_enqueue_style( 'style-bootstrap', get_template_directory_uri() .'/asstes/css/bootstrap.min.css' );
+wp_enqueue_script( 'script-js', get_template_directory_uri() . '/asstes/js/bootstrap.bundle.min.js', array(), '1.0.0', true );
 
-wp_enqueue_style( 'main-style', get_stylesheet_uri() );
-wp_enqueue_style( 'boostrap-style',  get_template_directory_uri() . '/assect/js/bootstrap.min.js' );
-wp_enqueue_script( 'script-name', get_template_directory_uri() .'assect/js/bootstrap.bundle.min.js', array(), '1.0.0', true );
+add_theme_support('post-thumbnails');
+add_theme_support( 'custom-logo' );
 
-add_theme_support( 'title-tag' );
-add_theme_support(  'custom-logo' );
-add_theme_support(  'post-thumbnails' );
+// 
+
+register_nav_menus([
+    'tm' => "primary"
+]);
+
+// 
+
+
+register_sidebar(array(
+    'name'=>'logo-2',
+    'id'=>'bdlogo',
+    'before_widget'  => '',
+	'after_widget'   => ''
+));
+
+// 
+
+register_sidebar(array(
+    'name'=>'h_title',
+    'id'=>'h_title',
+    'before_widget'  => '',
+	'after_widget'   => ''
+));
+
+//
+register_sidebar(array(
+    'name'=>'card',
+    'id'=>'card',
+    'before_widget'  => '',
+	'after_widget'   => ''
+));
 ?>
--- ThemePodma/index.php
+++ ThemePodma/index.php
@@ -4,35 +4,138 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-
-    <link rel="stylesheet" href="">
-    <?php wp_head(); ?>
-    
+    <title>Document</title>
+    <?php wp_head();?>
 </head>
 <body>
+    <header>
+        <section class="bennar">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+                <div class="carousel-inner">
+                    <?php
+                        $x=0;
+                        while (have_posts()) {the_post();
+                            $x++;
+                    ?>
+                    <div class="carousel-item active">
+                        <?php the_post_thumbnail();?>
+                    </div>
+                    <?php }?>
+                    <!-- <div class="carousel-item">
+                    <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="..." class="d-block w-100" alt="...">
+                    </div> -->
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </section>
 
-<header>
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <?php
-    $x=0;
-    while(have_posts()){the_post();
-      $x++;
-    ?>
-    <div class="carousel-item <?=($x==1)? 'active' : ''?>">
-      <?php the_post_thumbnail();?>
-      <img src="./assect/image/slide12 (1).jpg" class="d-block w-100" alt="...">
-    </div>
-    <?php }?>
-   
-  </div>
-</div>
-</header>
-    
+        <!-- logo -->
+
+        <section class="site_logo">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 text-start main-logo"><?php the_custom_logo();?></div>
+                    <div class="col-lg-6 text-end bd-logo"><?php dynamic_sidebar('bdlogo'); ?></div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- navber -->
+
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">              
+                <div class="collapse navbar-collapse" id="navbarNav">
+
+                <?php wp_nav_menu([
+                    'menu_location' => 'tm',
+                    'menu_class' => 'navbar-nav'
+
+                ]); ?>
+                <!-- <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul> -->
+                </div>
+            </div>
+        </nav>
+    </header>
+
+   <section class="container hero">
+        <div class="row hero-title">
+        <?php dynamic_sidebar('h_title');?>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+
+                    <?php dynamic_sidebar('card');?>
+
+                    <!-- <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div> -->
+                 </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+
+                    <?php dynamic_sidebar('card');?>
+
+                    <!-- <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div> -->
+                 </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+
+                    <?php dynamic_sidebar('card');?>
+
+                    <!-- <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div> -->
+                 </div>
+            </div>
+        </div>
+   </section>
+
+
+
 <!--  -->
-
-<?php wp_footer(); ?>
-
+<?php wp_footer();?>
 </body>
 </html>
